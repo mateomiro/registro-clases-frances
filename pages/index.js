78009d43e5bb52837380095d84df0ca89dfa7fd8
mateomiro <mateomiro@hotmail.com>
2025-01-22 16:20:50 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div style={{
+      padding: '20px',
+      maxWidth: '600px',
+      margin: '0 auto',
+      fontFamily: 'system-ui, sans-serif'
+    }}>
+      <h1 style={{
+        color: '#333',
+        textAlign: 'center'
+      }}>
+        Registro de Clases de Francés
+      </h1>
+    </div>
+  );
+}
